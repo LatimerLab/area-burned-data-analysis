b01d79c4be2e93af584828396aa928bcf203fe6e
amlatimer <amlatimer@ucdavis.edu>
2023-04-05 09:45:21 -0700

    start new script to fit power law distributions

---
+++ code/fit_distributions_to_area_burned.R
@@ -0,0 +1,6 @@
+#### Explore distribution of daily burned areas, test power law fits and changes over time #### 
+# Andrew Latimer 
+# Q: are there more "extreme spread events" EWEs recently? If so, are those simply the result of drawing more fire days from the same distribution, or is there a distribution shift that favors higher extremes? If so is there any clue why that's happening? 
+
+# Input to this script is the file of daily burned areas output from "extract_daily_area.R" in this repository 
+
